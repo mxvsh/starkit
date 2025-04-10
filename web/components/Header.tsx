65dbfa697633b9fbd9bb6471dd4780a9f82a3803
mxvsh <xencodes@icloud.com>
2025-04-10 10:22:50 +0530

    refactor: update Header component to use imported logo for improved asset management

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -1,13 +1,10 @@
+import logo from './starkit.svg';
 import { Input } from './ui/Input';
 
 function Header() {
   return (
     <div className="h-20 flex items-center px-10 justify-between border-b sticky top-0 bg-background/50 backdrop-blur-md z-50">
-      <img
-        draggable={false}
-        src="/starkit.svg"
-        className="w-10 aspect-square"
-      />
+      <img draggable={false} src={logo} className="w-10 aspect-square" />
       <div className="flex items-center gap-2">
         <Input placeholder="Search" className="w-60" />
       </div>
